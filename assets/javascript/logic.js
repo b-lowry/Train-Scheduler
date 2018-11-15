e5d86b21e30e1512fcfccbde81ab5e6328fdef5e
Ben <ben.lowry@outlook.comgit config --global user.email ben.lowry@outlook.com>
2018-11-14 19:50:18 -0600

    html and firebase added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyBtZMcHhnxwP1OBdjFo4E5yWp67koIBIjA",
+    authDomain: "train-scheduler-1de2b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1de2b.firebaseio.com",
+    projectId: "train-scheduler-1de2b",
+    storageBucket: "train-scheduler-1de2b.appspot.com",
+    messagingSenderId: "440763960417"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
